807f84504b2eae427aa7276550f73d1672999578
danielsalazar30 <daniel30-enero@hotmail.com>
2023-03-01 12:18:28 -0500

    clean code and validations

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,12 +14,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SudokuPage(),
+      home: const SudokuPage(),
     );
   }
 }
 
 class SudokuPage extends StatefulWidget {
+  const SudokuPage({super.key});
+
   @override
   _SudokuPageState createState() => _SudokuPageState();
 }
@@ -39,10 +43,10 @@ class _SudokuPageState extends State<SudokuPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sudoku'),
+        title: const Text('Sudoku'),
       ),
       body: Container(
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           children: <Widget>[
             Expanded(
@@ -52,47 +56,81 @@ class _SudokuPageState extends State<SudokuPage> {
                   int row = index ~/ 9;
                   int col = index % 9;
                   return Container(
-                    padding: EdgeInsets.all(2.0),
+                    padding: const EdgeInsets.all(2.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1.0, color: Colors.black),
+                    ),
                     child: Text(
                       sudoku[row][col] == 0 ? '' : sudoku[row][col].toString(),
-                      style: TextStyle(
+                      style: const TextStyle(
                         fontSize: 20.0,
                         fontWeight: FontWeight.bold,
                       ),
                       textAlign: TextAlign.center,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 1.0, color: Colors.black),
                     ),
                   );
                 }),
               ),
             ),
             Container(
-              padding: EdgeInsets.all(16.0),
+              padding: const EdgeInsets.all(16.0),
               child: ElevatedButton(
                 onPressed: () {
-                  var position = [4, 5];
+                  var position = [3, 3];
+                  var squareNumber = 0;
+                  // Identify the position where the number is gonna be
+                  // Identify first 3 squares
+                  if (position[0] >= 0 && position[0] <= 2) {
+                    if (position[1] >= 0 && position[1] <= 2) {
+                      squareNumber = 1;
+                    }
+                    if (position[1] >= 3 && position[1] <= 5) {
+                      squareNumber = 2;
+                    }
+                    if (position[1] >= 6 && position[1] <= 8) {
+                      squareNumber = 3;
+                    }
+                  }
+                  // Identify 3 to 6 squares
+                  if (position[0] >= 3 && position[0] <= 5) {
+                    if (position[1] >= 0 && position[1] <= 2) {
+                      squareNumber = 4;
+                    }
+                    if (position[1] >= 3 && position[1] <= 5) {
+                      squareNumber = 5;
+                    }
+                    if (position[1] >= 6 && position[1] <= 8) {
+                      squareNumber = 6;
+                    }
+                  }
+                  // Identify 6 to 9 squares
+                  if (position[0] >= 6 && position[0] <= 8) {
+                    if (position[1] >= 0 && position[1] <= 2) {
+                      squareNumber = 7;
+                    }
+                    if (position[1] >= 3 && position[1] <= 5) {
+                      squareNumber = 8;
+                    }
+                    if (position[1] >= 6 && position[1] <= 8) {
+                      squareNumber = 9;
+                    }
+                  }
+                  log("squareNumber");
+                  log(squareNumber.toString());
                   List<int> horizontalLine = [];
                   List<int> verticalLine = [];
                   for (var i = 0; i < sudoku.length; i++) {
-                    // log(sudoku[i].toString());
+                    // Horizontal line
                     if (i == position[0]) {
                       horizontalLine.addAll(sudoku[i]);
                     }
-                    // if (i == position[1]) {
-                    // log("Validacion linea vertical");
+                    // Vertical line
                     verticalLine.add(sudoku[i][position[1]]);
-                    // }
                   }
                   log("Linea horizontal");
                   log(horizontalLine.toString());
                   log("Linea Vertical");
                   log(verticalLine.toString());
-                  // for (var i = 0; i < sudoku.length; i++) {
-                  //   log(sudoku[i].toString());
-                  //   log(sudoku[i].toString());
-                  // }
                 },
                 child: const Text(
                   "Solve",
