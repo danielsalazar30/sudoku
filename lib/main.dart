b284e8752a40751c8615c258fe57213d8a42f3db
danielsalazar30 <daniel30-enero@hotmail.com>
2023-03-01 23:09:06 -0500

    square chosen to validate

--- lib/main.dart
+++ lib/main.dart
@@ -76,8 +76,9 @@ class _SudokuPageState extends State<SudokuPage> {
               padding: const EdgeInsets.all(16.0),
               child: ElevatedButton(
                 onPressed: () {
-                  var position = [3, 3];
+                  var position = [6, 2];
                   var squareNumber = 0;
+                  var squareChosen = [];
                   // Identify the position where the number is gonna be
                   // Identify first 3 squares
                   if (position[0] >= 0 && position[0] <= 2) {
@@ -85,31 +86,31 @@ class _SudokuPageState extends State<SudokuPage> {
                       squareNumber = 1;
                     }
                     if (position[1] >= 3 && position[1] <= 5) {
-                      squareNumber = 2;
+                      squareNumber = 4;
                     }
                     if (position[1] >= 6 && position[1] <= 8) {
-                      squareNumber = 3;
+                      squareNumber = 7;
                     }
                   }
                   // Identify 3 to 6 squares
                   if (position[0] >= 3 && position[0] <= 5) {
                     if (position[1] >= 0 && position[1] <= 2) {
-                      squareNumber = 4;
+                      squareNumber = 2;
                     }
                     if (position[1] >= 3 && position[1] <= 5) {
                       squareNumber = 5;
                     }
                     if (position[1] >= 6 && position[1] <= 8) {
-                      squareNumber = 6;
+                      squareNumber = 8;
                     }
                   }
                   // Identify 6 to 9 squares
                   if (position[0] >= 6 && position[0] <= 8) {
                     if (position[1] >= 0 && position[1] <= 2) {
-                      squareNumber = 7;
+                      squareNumber = 3;
                     }
                     if (position[1] >= 3 && position[1] <= 5) {
-                      squareNumber = 8;
+                      squareNumber = 7;
                     }
                     if (position[1] >= 6 && position[1] <= 8) {
                       squareNumber = 9;
@@ -126,7 +127,61 @@ class _SudokuPageState extends State<SudokuPage> {
                     }
                     // Vertical line
                     verticalLine.add(sudoku[i][position[1]]);
+                    // Square chosen
+                    if (i >= 0 && i <= 2) {
+                      if (squareNumber == 1) {
+                        for (var d = 0; d <= 2; d++) {
+                          squareChosen.add(sudoku[i][d]);
+                        }
+                      }
+                      if (squareNumber == 2) {
+                        for (var d = 3; d <= 5; d++) {
+                          squareChosen.add(sudoku[i][d]);
+                        }
+                      }
+                      if (squareNumber == 3) {
+                        for (var d = 6; d <= 8; d++) {
+                          squareChosen.add(sudoku[i][d]);
+                        }
+                      }
+                    }
+                    if (i >= 3 && i <= 5) {
+                      if (squareNumber == 4) {
+                        for (var d = 0; d <= 2; d++) {
+                          squareChosen.add(sudoku[i][d]);
+                        }
+                      }
+                      if (squareNumber == 5) {
+                        for (var d = 3; d <= 5; d++) {
+                          squareChosen.add(sudoku[i][d]);
+                        }
+                      }
+                      if (squareNumber == 6) {
+                        for (var d = 6; d <= 8; d++) {
+                          squareChosen.add(sudoku[i][d]);
+                        }
+                      }
+                    }
+                    if (i >= 6 && i <= 8) {
+                      if (squareNumber == 7) {
+                        for (var d = 0; d <= 2; d++) {
+                          squareChosen.add(sudoku[i][d]);
+                        }
+                      }
+                      if (squareNumber == 8) {
+                        for (var d = 3; d <= 5; d++) {
+                          squareChosen.add(sudoku[i][d]);
+                        }
+                      }
+                      if (squareNumber == 9) {
+                        for (var d = 6; d <= 8; d++) {
+                          squareChosen.add(sudoku[i][d]);
+                        }
+                      }
+                    }
                   }
+                  log("squareChosen");
+                  log(squareChosen.toString());
                   log("Linea horizontal");
                   log(horizontalLine.toString());
                   log("Linea Vertical");
